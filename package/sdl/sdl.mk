d0b5848c6228f816a98d36e92f44ba4adcf09edf
gameblabla <gameblabla@openmailbox.org>
2020-10-27 14:07:23 +0100

    Update SDL_Lite build to latest one with some A64 NEON changes.

--- package/sdl/sdl.mk
+++ package/sdl/sdl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SDL_VERSION = 6eb5aa2de7849a1a81ce76633b35e8fff63e5779
+SDL_VERSION = 6f31ea70423c3b1b8d79f5d272476d8b9df5876b
 SDL_SITE = $(call github,gameblabla,SDL_Lite,$(SDL_VERSION))
 SDL_INSTALL_STAGING = YES
 SDL_DEPENDENCIES = host-pkgconf
@@ -23,9 +23,14 @@ SDL_DEPENDENCIES += pulseaudio
 SDL_CONF_OPTS += PULSE=1
 endif
 
+# Enables ARMv8 + NEON code. For now though, it is not used internally by SDL
+ifeq ($(BR2_ARM_FPU_NEON_FP_ARMV8),y)
+SDL_CONF_OPTS += NEON_A64=1
+else
 # Only for ARM 32-bits
 ifeq ($(BR2_ARM_CPU_HAS_ARM)$(BR2_ARM_CPU_HAS_NEON),yy)
 SDL_CONF_OPTS += NEON=1
+endif
 endif
 
 SDL_CONF_OPTS += KMSDRM=1
